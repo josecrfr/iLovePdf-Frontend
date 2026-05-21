2b4069b0ad7a50f9bdf377b6e69362290097ecb7
Jose Cruz <josecruz8298@gmail.com>
2026-05-21 23:47:12 +0200

    compress

--- src/app/pages/compress-pdf/compress-pdf.component.ts
+++ src/app/pages/compress-pdf/compress-pdf.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FileDownloadUtil } from 'src/app/utils/file-download.util';
 
 @Component({
   selector: 'app-compress-pdf',
   templateUrl: './compress-pdf.component.html',
-  styleUrls: ['./compress-pdf.component.css']
+  styleUrls: ['./compress-pdf.component.css'],
 })
 export class CompressPdfComponent {
+  file: File | null = null;
+  targetKb: number | null = null;
 
+  onFileSelected(event: any) {
+    const selectedFile: File = event.target.files[0];
+
+    if (selectedFile) {
+      this.file = selectedFile;
+    }
+  }
+
+  async compressPdf() {
+    if (!this.file || !this.targetKb) return;
+
+    const formData = new FormData();
+
+    formData.append('file', this.file);
+    formData.append('targetKb', this.targetKb.toString());
+
+    try {
+      const response = await fetch('http://localhost:8080/api/pdf/compress', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error HTTP: ${response.status}`);
+      }
+
+      const blob = await response.blob();
+
+      FileDownloadUtil.downloadBlob(blob, 'compress-result.pdf');
+    } catch (error) {
+      console.error('Error al comprimir PDF:', error);
+    }
+  }
 }
